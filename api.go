cd573131b742d8bab99dfb316d98bf0faf905350
Kuvakin Sergey <kuvakin@gmail.com>
2017-08-31 22:30:49 +0300

    first commit

---
+++ api.go
@@ -0,0 +1,203 @@
+package vk
+
+import (
+	"encoding/json"
+	"errors"
+	"net/url"
+	"regexp"
+	"strconv"
+
+	"github.com/lafin/go-httpclient"
+)
+
+var accessToken string
+
+// GetAccessToken - get access toket for authorize on the vk.com
+func GetAccessToken(clientID, email, pass string) (string, error) {
+	if len(accessToken) > 0 {
+		return accessToken, nil
+	}
+
+	client := httpclient.Client()
+	data, err := httpclient.GetData(AuthURL + "/authorize?client_id=" + clientID + "&redirect_uri=https://oauth.vk.com/blank.html&display=mobile&scope=" + Permissions + "&response_type=token&v=" + APIVersion)
+	if err != nil {
+		return "", err
+	}
+
+	r := regexp.MustCompile("<form method=\"post\" action=\"(.*?)\">")
+	match := r.FindStringSubmatch(string(data))
+	urlStr := match[1]
+
+	r = regexp.MustCompile("<input type=\"hidden\" name=\"(.*?)\" value=\"(.*?)\" ?/?>")
+	matches := r.FindAllStringSubmatch(string(data), -1)
+
+	formData := url.Values{}
+	for _, val := range matches {
+		formData.Add(val[1], val[2])
+	}
+	formData.Add("email", email)
+	formData.Add("pass", pass)
+
+	response, err := client.PostForm(urlStr, formData)
+	if err != nil {
+		return "", err
+	}
+
+	r = regexp.MustCompile("__q_hash=.*?")
+	if r.MatchString(response.Request.URL.String()) {
+		data, err := httpclient.GetData(response.Request.URL.String())
+		if err != nil {
+			return "", err
+		}
+
+		r := regexp.MustCompile("<form method=\"post\" action=\"(.*?)\">")
+		match := r.FindStringSubmatch(string(data))
+		response, err = client.PostForm(match[1], url.Values{})
+		if err != nil {
+			return "", err
+		}
+	}
+
+	r = regexp.MustCompile("access_token=(.*?)&")
+	match = r.FindStringSubmatch(response.Request.URL.String())
+	if len(match) > 0 {
+		accessToken = match[1]
+		return accessToken, nil
+	}
+
+	return "", errors.New("can't find the access_token")
+}
+
+// GetPosts - get list of posts
+func GetPosts(groupID, count string) (*Posts, error) {
+	data, err := httpclient.GetData(APIURL + "/method/wall.get?owner_id=-" + groupID + "&count=" + count + "&filter=all&access_token=" + accessToken + "&v=" + APIVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	var posts Posts
+	if err := json.Unmarshal(data, &posts); err != nil {
+		return nil, err
+	}
+	return &posts, nil
+}
+
+// GetGroupsInfo - get group info
+func GetGroupsInfo(groupIDs, fields string) (*Groups, error) {
+	data, err := httpclient.GetData(APIURL + "/method/groups.getById?group_ids=" + groupIDs + "&fields=" + fields + "&v=" + APIVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	var groups Groups
+	if err := json.Unmarshal(data, &groups); err != nil {
+		return nil, err
+	}
+	return &groups, nil
+}
+
+// DoRepost - do repost the post
+func DoRepost(object string, groupID int, message string) (*ResponseRepost, error) {
+	data, err := httpclient.GetData(APIURL + "/method/wall.repost?object=" + object + "&group_id=" + strconv.Itoa(groupID) + "&message=" + message + "&access_token=" + accessToken + "&v=" + APIVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	var repost ResponseRepost
+	if err := json.Unmarshal(data, &repost); err != nil {
+		return nil, err
+	}
+	return &repost, nil
+}
+
+// DoPost - do post the post
+func DoPost(groupID int, attachments, message string) (*ResponsePost, error) {
+	data, err := httpclient.GetData(APIURL + "/method/wall.post?owner_id=-" + strconv.Itoa(groupID) + "&from_group=1&mark_as_ads=0&attachments=" + attachments + "&message=" + message + "&access_token=" + accessToken + "&v=" + APIVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	var post ResponsePost
+	if err := json.Unmarshal(data, &post); err != nil {
+		return nil, err
+	}
+	return &post, nil
+}
+
+// GetMaxCountLikes - return max likes in list of posts
+func (p *Posts) GetMaxCountLikes() float32 {
+	max := 0
+	for _, item := range p.Response.Items {
+		if item.Likes.Count > max && item.IsPinned == 0 {
+			max = item.Likes.Count
+		}
+	}
+	return float32(max)
+}
+
+// GetUniqueFiles - get lists of files
+func (p *Post) GetUniqueFiles() ([][]byte, []string) {
+	var attachments []string
+	var files [][]byte
+
+	for _, item := range p.Attachments {
+		var err error
+		var file []byte
+		var attachment string
+		var photoURL string
+
+		switch item.Type {
+		case "photo":
+			if len(item.Photo.Photo75) > 0 {
+				photoURL = item.Photo.Photo75
+				attachment = item.Type + strconv.Itoa(item.Photo.OwnerID) + "_" + strconv.Itoa(item.Photo.ID) + "_" + item.Photo.AccessKey
+			}
+		case "doc":
+			if len(item.Doc.URL) > 0 {
+				photoURL = item.Doc.Preview.GetSmallPreview()
+				attachment = item.Type + strconv.Itoa(item.Doc.OwnerID) + "_" + strconv.Itoa(item.Doc.ID) + "_" + item.Doc.AccessKey
+			}
+		case "video":
+			attachment = item.Type + strconv.Itoa(item.Video.OwnerID) + "_" + strconv.Itoa(item.Video.ID) + "_" + item.Video.AccessKey
+		}
+		if photoURL != "" {
+			file, err = httpclient.GetData(photoURL)
+		}
+		if err != nil {
+			return nil, nil
+		}
+		if file != nil {
+			files = append(files, file)
+		}
+		attachments = append(attachments, attachment)
+	}
+	return files, attachments
+}
+
+// GetListUsersofGroup - get list deactivated users
+func GetListUsersofGroup(groupID int, offset int, count int) (*ResponseUsersOfGroup, error) {
+	data, err := httpclient.GetData(APIURL + "/method/groups.getMembers?group_id=" + strconv.Itoa(groupID) + "&offset=" + strconv.Itoa(offset) + "&count=" + strconv.Itoa(count) + "&fields=last_seen&v=" + APIVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	var users ResponseUsersOfGroup
+	if err := json.Unmarshal(data, &users); err != nil {
+		return nil, err
+	}
+	return &users, nil
+}
+
+// RemoveUserFromGroup - remove user from group
+func RemoveUserFromGroup(groupID int, userID int) (*ResponseRemoveUser, error) {
+	data, err := httpclient.GetData(APIURL + "/method/groups.removeUser?group_id=" + strconv.Itoa(groupID) + "&user_id=" + strconv.Itoa(userID) + "&access_token=" + accessToken + "&v=" + APIVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	var status ResponseRemoveUser
+	if err := json.Unmarshal(data, &status); err != nil {
+		return nil, err
+	}
+	return &status, nil
+}
---
+++ api_events.go
@@ -0,0 +1,206 @@
+package vk
+
+// Entripoints for the vk.com
+const (
+	AuthURL     = "https://oauth.vk.com"
+	APIURL      = "https://api.vk.com"
+	APIVersion  = "5.50"
+	Permissions = "wall,groups,friends,photos,video,docs"
+)
+
+// Groups - struct of json object the Groups
+type Groups struct {
+	Response []Group
+}
+
+// Group - struct of json object the Group
+type Group struct {
+	AdminLevel int `json:"admin_level"`
+	ID         int `json:"id"`
+	IsAdmin    int `json:"is_admin"`
+	IsClosed   int `json:"is_closed"`
+	IsMember   int `json:"is_member"`
+	Links      []struct {
+		ID       int    `json:"id"`
+		Name     string `json:"name"`
+		Photo100 string `json:"photo_100"`
+		Photo50  string `json:"photo_50"`
+		URL      string `json:"url"`
+	} `json:"links"`
+	Name       string `json:"name"`
+	Photo100   string `json:"photo_100"`
+	Photo200   string `json:"photo_200"`
+	Photo50    string `json:"photo_50"`
+	ScreenName string `json:"screen_name"`
+	Type       string `json:"type"`
+}
+
+type DocPreview struct {
+	Photo struct {
+		Sizes []struct {
+			Height int    `json:"height"`
+			Src    string `json:"src"`
+			Type   string `json:"type"`
+			Width  int    `json:"width"`
+		} `json:"sizes"`
+	} `json:"photo"`
+	Video struct {
+		FileSize int    `json:"file_size"`
+		Height   int    `json:"height"`
+		Src      string `json:"src"`
+		Width    int    `json:"width"`
+	} `json:"video"`
+}
+
+// GetSmallPreview - return preview with type "s" for gif's
+func (r *DocPreview) GetSmallPreview() string {
+	sizes := r.Photo.Sizes
+	for i := 0; i < len(sizes); i++ {
+		size := sizes[i]
+		if size.Type == "s" {
+			return size.Src
+		}
+	}
+	return ""
+}
+
+// Post - struct of json object the Item
+type Post struct {
+	Attachments []struct {
+		Video struct {
+			AccessKey   string `json:"access_key"`
+			CanAdd      int    `json:"can_add"`
+			Comments    int    `json:"comments"`
+			Date        int    `json:"date"`
+			Description string `json:"description"`
+			Duration    int    `json:"duration"`
+			ID          int    `json:"id"`
+			OwnerID     int    `json:"owner_id"`
+			Photo130    string `json:"photo_130"`
+			Photo320    string `json:"photo_320"`
+			Photo640    string `json:"photo_640"`
+			Platform    string `json:"platform"`
+			Title       string `json:"title"`
+			Views       int    `json:"views"`
+		} `json:"video"`
+		Photo struct {
+			AccessKey string `json:"access_key"`
+			AlbumID   int    `json:"album_id"`
+			Date      int    `json:"date"`
+			Height    int    `json:"height"`
+			ID        int    `json:"id"`
+			OwnerID   int    `json:"owner_id"`
+			Photo130  string `json:"photo_130"`
+			Photo604  string `json:"photo_604"`
+			Photo75   string `json:"photo_75"`
+			Text      string `json:"text"`
+			UserID    int    `json:"user_id"`
+			Width     int    `json:"width"`
+		} `json:"photo"`
+		Doc struct {
+			AccessKey string `json:"access_key"`
+			Date      int    `json:"date"`
+			Ext       string `json:"ext"`
+			ID        int    `json:"id"`
+			OwnerID   int    `json:"owner_id"`
+			Preview   DocPreview
+			Size      int    `json:"size"`
+			Title     string `json:"title"`
+			Type      int    `json:"type"`
+			URL       string `json:"url"`
+		} `json:"doc"`
+		Type string `json:"type"`
+	} `json:"attachments"`
+	Comments struct {
+		CanPost int `json:"can_post"`
+		Count   int `json:"count"`
+	} `json:"comments"`
+	Date     int `json:"date"`
+	FromID   int `json:"from_id"`
+	ID       int `json:"id"`
+	IsPinned int `json:"is_pinned"`
+	Likes    struct {
+		CanLike    int `json:"can_like"`
+		CanPublish int `json:"can_publish"`
+		Count      int `json:"count"`
+		UserLikes  int `json:"user_likes"`
+	} `json:"likes"`
+	OwnerID    int `json:"owner_id"`
+	PostSource struct {
+		Type string `json:"type"`
+	} `json:"post_source"`
+	PostType string `json:"post_type"`
+	Reposts  struct {
+		Count        int `json:"count"`
+		UserReposted int `json:"user_reposted"`
+	} `json:"reposts"`
+	Text string `json:"text"`
+}
+
+// Posts - struct of json object the Posts
+type Posts struct {
+	Response struct {
+		Count int    `json:"count"`
+		Items []Post `json:"items"`
+	} `json:"response"`
+}
+
+// ResponseRepost - struct of response after repost of post
+type ResponseRepost struct {
+	Response struct {
+		LikesCount   int `json:"likes_count"`
+		PostID       int `json:"post_id"`
+		RepostsCount int `json:"reposts_count"`
+		Success      int `json:"success"`
+	} `json:"response"`
+}
+
+// ResponsePost - struct of response after post of post
+type ResponsePost struct {
+	Error struct {
+		ErrorCode     int    `json:"error_code"`
+		ErrorMsg      string `json:"error_msg"`
+		RequestParams []struct {
+			Key   string `json:"key"`
+			Value string `json:"value"`
+		} `json:"request_params"`
+	} `json:"error"`
+	Response struct {
+		PostID int `json:"post_id"`
+	} `json:"response"`
+}
+
+// ResponseUsersOfGroup - struct of response list users of group
+type ResponseUsersOfGroup struct {
+	Response struct {
+		Count int                   `json:"count"`
+		Items []ResponseUserOfGroup `json:"items"`
+	} `json:"response"`
+}
+
+// ResponseUserOfGroup - struct of response list user of group
+type ResponseUserOfGroup struct {
+	FirstName   string `json:"first_name"`
+	ID          int    `json:"id"`
+	LastName    string `json:"last_name"`
+	Deactivated string `json:"deactivated"`
+	LastSeen    struct {
+		Platform int `json:"platform"`
+		Time     int `json:"time"`
+	} `json:"last_seen"`
+}
+
+// IsBanned - returned true if the user was banned
+func (r *ResponseUserOfGroup) IsBanned() bool {
+	return r.Deactivated == "banned"
+}
+
+// IsDeleted - returned true if the user was deleted
+func (r *ResponseUserOfGroup) IsDeleted() bool {
+	return r.Deactivated == "deleted"
+}
+
+// ResponseRemoveUser - struct of response status of removing the user
+type ResponseRemoveUser struct {
+	Response int `json:"response"`
+}
